0bdb5ffa20aec528b37aa5234b3c65897ea86bc6
SaiKrishna6699 <saikrishnarajagopal@gmail.com>
2024-01-01 02:44:05 +0530

    v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorkComponent } from './components/work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/work', pathMatch: 'full' },
+  { path: 'work', component: WorkComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

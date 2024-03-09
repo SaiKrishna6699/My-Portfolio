78cf07b9c41e87453386a2b8c922810ef8f1069c
SaiKrishna6699 <saikrishnarajagopal@gmail.com>
2024-03-09 21:43:38 +0530

    Made changes for footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { WorkComponent } from './components/work/work.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    WorkComponent
+    WorkComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
